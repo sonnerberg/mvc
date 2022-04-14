5fc35276b366e66401dfb4a2cbccc6f88e91b1c5
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2022-04-14 18:34:34 +0200

    Change name from build to dist

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 
 Encore
     // directory where compiled assets will be stored
-    .setOutputPath('/build/')
+    .setOutputPath('public/dist/')
     // public path used by the web server to access the output path
-    .setPublicPath('/build')
+    .setPublicPath('/dist')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
