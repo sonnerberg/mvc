71964ab669ee2afb1e9033d91b44e05ebe97b11b
Richard Sönnerberg <31686265+sonnerberg@users.noreply.github.com>
2022-04-20 11:35:16 +0200

    Add POST card/api/deck/draw

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -5,19 +5,47 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController extends AbstractController
 {
     #[Route('/card/api/deck/shuffle', name: 'api_shuffle', methods: ['POST'])]
-    public function index(): JsonResponse
-    {
+    public function index(
+        SessionInterface $session,
+
+    ): JsonResponse {
         $deck = new \App\Card\CardDeck();
         $deck->populateDeck();
         $deck->shuffleDeck();
 
+        $session->set("card-hand", $deck);
+
         return $this->json([
             'data' => ['deck' => $deck->getAsString()],
         ]);
+    }
+
+    #[Route('/card/api/deck/draw/{number}', name: 'api_draw', methods: ['POST'])]
+    public function drawCards(
+        SessionInterface $session,
+        int $number = 1
+    ): JsonResponse {
+        $deck = $session->get("card-hand") ?? new \App\Card\CardDeck();
+        if (!$session->get("card-hand")) {
+            $deck->populateDeck();
+        }
+        $deck->drawACardAndAddItToHand($number);
+
+        $session->set("card-hand", $deck);
+
+        $data = [
+            'title' => 'Card',
+            'deck' => $deck->getAsString(),
+            'amount_cards' => $deck->getAmountOfCardsInDeck(),
+            'cardsDrawn' => $deck->getDrawnCardsAsString(),
+        ];
+
+        return $this->json($data);
     }
 }
